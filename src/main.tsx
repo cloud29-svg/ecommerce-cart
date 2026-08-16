b7dbaa2ff4f207c1fb9889a33d346f58e9e9c8b2
precious Inya <chijiokeprecious79@gmail.com>
2026-08-16 01:36:32 +0100

    Remove temporary debug line used for manual Redux testing

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import { store } from "./app/store"; // adjust path to wherever your store is
+import { store } from "./app/store"; 
 import App from "./App.tsx";
 import "./index.css";
 
